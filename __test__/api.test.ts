10343a1c9709e1eb0cda4f662ae0f21e56bbb7d4
Eduardo da Silva <eduardosilva.pn@gmail.com>
2023-09-20 22:21:45 -0300

    add babel

--- __test__/api.test.ts
+++ __test__/api.test.ts
@@ -1,3 +1,13 @@
-test("teste", function () {
-  expect(1 + 1).toBe(2);
+import setupAPI from "../src/api/index";
+import request from "supertest";
+
+describe("API Endpoint", () => {
+  beforeAll(async () => {
+    await setupAPI();
+  });
+
+  it("Root Endpoint works", async () => {
+    const res = await request("localhost:3000").get("/").send();
+    expect(res.statusCode).toBe(200);
+  });
 });
