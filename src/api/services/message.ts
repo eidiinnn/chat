992e2645d9386753c92553a682782fe7650d64a4
Eduardo da Silva <eduardosilva.pn@gmail.com>
2023-09-26 20:38:50 -0300

    return 401 on unauthorized

--- src/api/services/message.ts
+++ src/api/services/message.ts
@@ -6,7 +6,7 @@ function setupMessageAPI(app: Express): void {
       const { token, message } = req.body
       void UserModel.findOne({ token }).exec().then(async user => {
          if (!user) {
-            res.sendStatus(400)
+            res.sendStatus(401)
          } else {
             const data = await new MessageModel({ createdDate: new Date().toString(), message, user: user._id }).save()
             if (data._id) res.send({ message: data.message, user: data.user })
